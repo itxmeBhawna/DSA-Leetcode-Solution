817fe1fd8c3c04e3778a295f37af500c40740f10
itxmeBhawna <bhawnakumari9599@gmail.com>
2026-02-17 21:10:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.45%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        left = 0
+        right = len(nums)-1
+        mid = 0
+        while mid<=right:
+            if nums[mid]==0:
+                nums[mid],nums[left] = nums[left], nums[mid]
+                left+=1
+                mid+=1
+            elif nums[mid] == 1:
+                mid+=1
+            else:
+                nums[mid],nums[right] = nums[right], nums[mid]
+                right -=1 
+                        
+
+        
+
+       
+        
