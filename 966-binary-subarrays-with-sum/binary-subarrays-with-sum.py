d3cc390068eff160d7b6d59939ce79c0166c5369
itxmeBhawna <bhawnakumari9599@gmail.com>
2026-02-25 12:27:21 +0530

    Time: 31 ms (74.30%) | Memory: 23.1 MB (29.84%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         seen = {0:1}
-        add = 0
+        cur_sum = 0
         count= 0
         for num in nums:
-            add += num
-            if add - goal in seen:
-                count += seen[add-goal]
-            seen[add] = seen.get(add, 0)+1
+            cur_sum += num
+            if cur_sum - goal in seen:
+                count += seen[cur_sum-goal]
+            seen[cur_sum] = seen.get(cur_sum, 0)+1
         return count
         
