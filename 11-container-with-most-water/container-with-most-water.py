1467a338a9f7f90cb557b387a9f1fa6b9e4ee0ad
Bhawna Kumari <bhawnakumari9599@gmail.com>
2026-06-10 20:08:29 +0530

    Time: 64 ms (17.00%) | Memory: 29.6 MB (68.03%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,15 +4,17 @@ class Solution:
         right = len(height) - 1
         max_area = 0
         while left < right:
-            wighth = right - left
+            width = right - left
             water_height = min(height[left], height[right])
-            area = wighth * water_height
+            area = width * water_height
             max_area = max(max_area, area)
-            if height[left]<height[right]:
+            if height[left] < height[right]:
                 left +=1
             else:
                 right -=1
-        return max_area            
+        return max_area           
+
+               
 
        
             
