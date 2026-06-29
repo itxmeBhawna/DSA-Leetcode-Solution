ab1216fc3db920e422d111f2b992c0905d8b1d31
Bhawna Kumari <bhawnakumari9599@gmail.com>
2026-06-29 18:40:55 +0530

    Time: 28 ms (24.52%) | Memory: 20.5 MB (31.42%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,6 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         far = 0
+        if far >= len(nums) - 1:
+            return True
         for i in range(len(nums)):
             if i > far:
                 return False
