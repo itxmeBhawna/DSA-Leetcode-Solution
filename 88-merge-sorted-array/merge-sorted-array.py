1d42d2facbf04a307ff56fccb5c958272a481875
Bhawna Kumari <bhawnakumari9599@gmail.com>
2026-07-20 16:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (8.56%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,18 +7,15 @@ class Solution:
         i = m-1
         j = n-1
         while(i>=0 and j>=0):
-            if nums1[i] <= nums2[j]:
-                nums1[k] = nums2[j]
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
                 k = k-1
-                j = j-1
-            elif(nums1[i]>nums2[j]):
-                nums1[k]=nums1[i]
+                i = i-1
+            else:
+                nums1[k]=nums2[j]
                 k -=1
-                i -=1
-        # while i>=0:
-        #     nums1[k] = nums1[i]
-        #     i -=1
-        #     k -=1
+                j -=1
+
         while j>=0:
             nums1[k]=nums2[j]
             j-=1
