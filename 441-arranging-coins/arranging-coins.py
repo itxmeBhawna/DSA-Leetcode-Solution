1d9ab84abef7e24ced511eb3c5d2a7c9ab62e8c2
Bhawna Kumari <bhawnakumari9599@gmail.com>
2026-09-18 11:15:52 +0530

    Time: 3 ms (69.20%) | Memory: 19.1 MB (89.24%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,11 +1,17 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        i = 1
-        stair = 0
-        while i<= n:
-            n-=i
-            stair +=1
-            i+=1
-        return stair    
+        left = 1
+        right = ceil(n/2)
+        ans = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            coin_need = ceil(mid*(mid + 1)/2)
+            if coin_need <= n:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans            
+        
 
             
